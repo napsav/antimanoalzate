5b7dad4f91ac46f2a89dd68a7de9a24c819bbb00
napsav <savi.napolitano@gmail.com>
2021-01-10 14:59:59 +0100

    Commit iniziale

---
+++ server/server.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "fmt"
+	"log"
+	"net"
+	"net/http"
+	"github.com/go-vgo/robotgo"
+)
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+func upHandler(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+    if r.URL.Path != "/up" {
+        http.Error(w, "404 non trovato.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Solo richieste GET.", http.StatusNotFound)
+        return
+	}
+	robotgo.KeyTap("up")
+    fmt.Fprintf(w, "up!")
+}
+
+func downHandler(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+    if r.URL.Path != "/down" {
+        http.Error(w, "404 non trovato.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Solo richieste GET.", http.StatusNotFound)
+        return
+    }
+	robotgo.KeyTap("down")
+    fmt.Fprintf(w, "down!")
+}
+
+func main() {
+	http.HandleFunc("/up", upHandler)
+	http.HandleFunc("/down", downHandler)
+	fmt.Printf("Server in ascolto alla porta 8080\n")
+	fmt.Printf("Nell'app inserisci l'indirizzo qui sotto che inizia per '192.168.1.xxx' o '192.168.0.xxx'\n")
+	fmt.Printf("------------------------------------\n")
+	ifaces, err := net.Interfaces()
+if err != nil{
+	log.Fatal(err)
+}
+for _, i := range ifaces {
+    addrs,err := i.Addrs()
+    if err != nil{
+		log.Fatal(err)
+	}
+    for _, addr := range addrs {
+        var ip net.IP
+        switch v := addr.(type) {
+        case *net.IPNet:
+                ip = v.IP
+        case *net.IPAddr:
+                ip = v.IP
+        }
+        fmt.Printf("Possibile indirizzo ip: "+ip.String()+"\n")
+    }
+}
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
+
---
+++ www/js/index.js
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.querySelector('body').classList.remove('hidden');
+}
+
+document.getElementById("save").addEventListener('click', salvaImpostazioni, false)
+let pronto = false;
+let ipSalvato = "";
+let portaSalvata = "8080";
+function salvaImpostazioni() {
+		let errors = [];
+		let ip = document.getElementById('indirizzoip').value
+		let porta = document.getElementById('porta').value
+		let ipPattern = new RegExp(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)
+		if(ipPattern.test(ip)) {
+			document.getElementById('pronto').classList.remove('hidden')
+			pronto = true
+			ipSalvato = ip
+		} else {
+			errors.push("Indirizzo IP non valido")
+		}
+		if (errors.length > 0) {
+			for(elem of errors) {
+				let err = document.createElement("p")
+				err.innerHTML = elem
+				err.style="color:red"
+				document.body.append(err)
+			}
+		}
+}
+
+
+let log = document.getElementById("log")
+
+	document.addEventListener("volumeupbutton", volumeUp, false)
+	document.addEventListener("volumedownbutton", volumeDown, false)
+
+
+function httpGetAsync(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            console.log(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function volumeUp() {
+	log.innerHTML += "volume sopra\n"
+	httpGetAsync("http://"+ipSalvato+":"+portaSalvata+"/up")
+}
+function volumeDown() {
+	log.innerHTML += "volume sotto\n"
+	httpGetAsync("http://"+ipSalvato+":"+portaSalvata+"/down")
+}
